1b9794b0cfd2a550d32ea517902afc853deed86e
rcgueck <ryangueck@yahoo.com>
2020-03-01 20:51:43 -0600

    added functions for sign in routes
    need to update each function but requires more in depth looks into task 5

--- src/routes/signInRoutes.ts
+++ src/routes/signInRoutes.ts
@@ -3,9 +3,13 @@ import { RouteLookup } from "../controllers/lookups/routingLookup";
 import * as SignInRouteController from "../controllers/signInRouteController";
 
 function signInRoutes(server: express.Express) {
-	// TODO: Route for initial page load
+	server.get(
+		RouteLookup.SignIn,
+		SignInRouteController.start);
 
-	server.post(RouteLookup.SignIn, SignInRouteController.signIn);
+	server.post(
+		(RouteLookup.API + RouteLookup.SignIn),
+		SignInRouteController.signIn);
 
 	server.delete(
 		(RouteLookup.API + RouteLookup.SignOut),
