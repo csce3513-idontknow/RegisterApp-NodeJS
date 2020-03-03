ad46f284d927f46f3c469d1da12c72824d522b27
rcgueck <ryangueck@yahoo.com>
2020-03-03 00:24:29 -0600

    Added a few things to sign in route controller

--- src/controllers/signInRouteController.ts
+++ src/controllers/signInRouteController.ts
@@ -1,11 +1,18 @@
-// beginning sign in route controller
 import { Request, Response } from "express";
+import { Resources } from "../resourceLookup";
+import * as Helper from "./helpers/routeControllerHelper";
 import { ViewNameLookup } from "./lookups/routingLookup";
+import * as EmployeeSignIn from "./commands/employees/employeeSignInCommand";
+import { PageResponse, CommandResponse, Employee, SignInPageResponse } from "./typeDefinitions";
 
 export const start = async (req: Request, res: Response): Promise<void> => {
 	// TODO: Use the credentials provided in the request body (req.body)
 	//  and the "id" property of the (Express.Session)req.session variable
 	//  to sign in the user
+	if (Helper.handleInvalidSession(req, res)) {
+		return;
+	}
+
 	return res.render(
 		ViewNameLookup.SignIn,
 	)
