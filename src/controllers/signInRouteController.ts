69ea9a21b9b565c4d9fa95749a5ddb4b9884c23a
rcgueck <ryangueck@yahoo.com>
2020-03-02 20:24:25 -0600

    switched to sprint2SignIn branch

--- src/controllers/signInRouteController.ts
+++ src/controllers/signInRouteController.ts
@@ -1,9 +1,18 @@
+// beginning sign in route controller
 import { Request, Response } from "express";
+import { ViewNameLookup } from "./lookups/routingLookup";
 
-export const signIn = async (req: Request, res: Response): Promise<void> => {
+export const start = async (req: Request, res: Response): Promise<void> => {
 	// TODO: Use the credentials provided in the request body (req.body)
 	//  and the "id" property of the (Express.Session)req.session variable
 	//  to sign in the user
+	return res.render(
+		ViewNameLookup.SignIn,
+	)
+};
+
+export const signIn = async (req: Request, res: Response): Promise<void> => {
+	//verify employee credentials
 };
 
 export const clearActiveUser = async (req: Request, res: Response): Promise<void> => {
