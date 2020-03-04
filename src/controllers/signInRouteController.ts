9014cb8698c37654edc663e90e9482420292c224
turnersawyer <tsawyer@uark.edu>
2020-03-03 19:19:24 -0600

    Forgot this file

--- src/controllers/signInRouteController.ts
+++ src/controllers/signInRouteController.ts
@@ -1,30 +1,65 @@
 import { Request, Response } from "express";
 import { Resources } from "../resourceLookup";
 import * as Helper from "./helpers/routeControllerHelper";
-import { ViewNameLookup } from "./lookups/routingLookup";
+import { ViewNameLookup, RouteLookup } from "./lookups/routingLookup";
 import * as EmployeeSignIn from "../controllers/commands/employees/employeeSignInCommand";
 import { PageResponse, CommandResponse, Employee } from "./typeDefinitions";
-import * as EmployeeExists from "../controllers/commands/employees/activeEmployeeExistsQuery";
+import * as ActiveEmployeeExists from "../controllers/commands/employees/activeEmployeeExistsQuery";
+import * as ClearActiveUser from "./commands/activeUsers/clearActiveUserCommand";
+import { SignInRequest, ApiResponse } from "./typeDefinitions";
 
 export const start = async (req: Request, res: Response): Promise<void> => {
 	// TODO: Use the credentials provided in the request body (req.body)
 	//  and the "id" property of the (Express.Session)req.session variable
 	//  to sign in the user
-	if (Helper.handleInvalidSession(req, res)) {
-		return;
+	try {
+		if((await ActiveEmployeeExists.execute()).data === false) {
+			return res.redirect(RouteLookup.EmployeeDetail);
+		}
+		return res.render(
+			ViewNameLookup.SignIn,
+		)
+	} catch(e) {
+		console.log("Error: " + e.toString());
+		res.sendStatus(500);
 	}
-
-	return res.render(
-		ViewNameLookup.SignIn,
-	)
 };
 
 export const signIn = async (req: Request, res: Response): Promise<void> => {
 	//verify employee credentials
-	console.log("HERE");
+	// console.log("HERE");
+	// return res.render(
+	// 	ViewNameLookup.SignIn,
+	// )
+	try {
+		if(!req.session) {
+		}
+		await EmployeeSignIn.signInQuery(<SignInRequest>{
+			employeeId: req.body.employeeId,
+			password: req.body.password
+		}, req.session);
+		return res.redirect(RouteLookup.MainMenu);
+	} catch(e) {
+		res.status(e.status).render(ViewNameLookup.SignIn, <ApiResponse>{
+			errorMessage: e.message
+		});
+	}
 
 };
 
 export const clearActiveUser = async (req: Request, res: Response): Promise<void> => {
 	// TODO: Sign out the user associated with req.session.id
+	try {
+		if(req.session) {
+			await ClearActiveUser.execute(req.session.id);
+		}
+		res.status(204).send(<ApiResponse>{
+			redirectUrl: RouteLookup.SignIn
+		});
+	} catch(e) {
+		res.status(e.status).send(<ApiResponse>{
+			redirectUrl: RouteLookup.SignIn,
+			errorMessage: e.message
+		});
+	}
 };
