bc600c6e02386c69f5941182438afaaf480c9942
Alessandra Garcia <60279216+alessandragarcia18@users.noreply.github.com>
2020-03-03 22:01:03 -0600

    product changes

--- src/controllers/productDetailRouteController.ts
+++ src/controllers/productDetailRouteController.ts
@@ -2,10 +2,12 @@ import { Request, Response } from "express";
 import { Resources, ResourceKey } from "../resourceLookup";
 import * as ProductQuery from "./commands/products/productQuery";
 import { ViewNameLookup, ParameterLookup, RouteLookup } from "./lookups/routingLookup";
+import * as ValidateActiveUser from "./commands/activeUsers/validateActiveUserCommand";
 import * as ProductCreateCommand from "./commands/products/productCreateCommand";
 import * as ProductDeleteCommand from "./commands/products/productDeleteCommand";
 import * as ProductUpdateCommand from "./commands/products/productUpdateCommand";
-import { CommandResponse, Product, ProductDetailPageResponse, ApiResponse, ProductSaveResponse, ProductSaveRequest } from "./typeDefinitions";
+import { EmployeeClassification } from "./commands/models/constants/entityTypes";
+import { CommandResponse, Product, ProductDetailPageResponse, ApiResponse, ProductSaveResponse, ProductSaveRequest, ActiveUser } from "./typeDefinitions";
 
 const processStartProductDetailError = (res: Response, error: any): void => {
 	let errorMessage: (string | undefined) = "";
@@ -45,6 +47,19 @@ const saveProduct = async (
 	performSave: (productSaveRequest: ProductSaveRequest) => Promise<CommandResponse<Product>>
 ): Promise<void> => {
 
+	ValidateActiveUser.execute((<Express.Session>req.session).id)
+		.then((activeUserCommandResponse: CommandResponse<ActiveUser>): void => {
+			// TODO: Examine the ActiveUser classification if you want this information
+			let isElevatedUser: boolean = false;
+			if ( activeUserCommandResponse.data?.classification == EmployeeClassification.GeneralManager ) {
+				isElevatedUser = true;
+			}
+			else if ( activeUserCommandResponse.data?.classification == EmployeeClassification.ShiftManager ) {
+				isElevatedUser = true;
+			}
+			else if ( activeUserCommandResponse.data?.classification == EmployeeClassification.NotDefined ) { //not elevated user 
+				res.redirect( RouteLookup.ProductListing, 401 ); 
+			}});
 	return performSave(req.body)
 		.then((createProductCommandResponse: CommandResponse<Product>): void => {
 			res.status(createProductCommandResponse.status)
