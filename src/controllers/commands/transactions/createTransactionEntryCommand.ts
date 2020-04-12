94859b602d3160739b75aa1d45366ca54fdd0e7a
rcgueck <ryangueck@yahoo.com>
2020-04-11 20:23:32 -0500

    finished create Transaction Entry Command

--- src/controllers/commands/transactions/createTransactionEntryCommand.ts
+++ src/controllers/commands/transactions/createTransactionEntryCommand.ts
@@ -12,7 +12,36 @@ export const execute = async (transactionId: string, productCode: string) => {
                 status: 404,
                 message: Resources.getString(ResourceKey.TRANSACTION_UNABLE_TO_ADD)
             };
+
         const product = await queryByLookupCode(productCode);
+        if (!product)
+            return <CommandResponse<TransactionEntry>>{
+                status: 404,
+                message: Resources.getString(ResourceKey.TRANSACTION_UNABLE_TO_ADD)
+            };
         
+        const entry = await TransactionEntryModel.create({
+            price: product.price,
+            quantity: 1,
+            productId: product.id,
+            transactionId
+        });
+
+        return <CommandResponse<TransactionEntry>>{
+            status: 201,
+            data: <TransactionEntry>{
+                id: entry.id,
+                price:entry.price,
+                quantity: entry.quantity,
+                productId: entry.productId,
+                transactionId: entry.transactionId,
+                createdOn: entry.createdOn
+            }
+        };
+    }catch (error) {
+        throw <CommandResponse<TransactionEntry>>{
+            status: error.status ?? 500,
+            message: error.message ?? Resources.getString(ResourceKey.TRANSACTION_UNABLE_TO_ADD)
+        };
     }
-}
+};
